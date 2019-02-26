cd001945c03be27916663c790f0b7a703632e2e1
John Connolly <JTConnolly82@gmail.com>
2019-02-25 20:58:20 -0700

    end of lecture

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,51 @@
+class PagesController < ApplicationController
+  def index
+    @pages = Page.all #SQL: SELECT * FROM PAGE
+    # redirected views method name
+  end
+
+  def show
+    @page = Page.find(params[:id])
+  end
+
+  def new
+    # Page.create <- stores in database
+    @page = Page.new # stores in memory
+  end
+
+def create
+  @page = Page.new(pages_params)
+
+  if @page.save
+    redirect_to pages_path
+  else
+    render :new
+  end
+end
+
+def edit
+  @page = Page.find(params[:id])
+end
+
+def update
+  @page = Page.find(params[:id])
+  if @page.update(pages_params)
+    redirect_to pages_path
+  else
+    render :edit
+  end
+end
+
+def destroy
+
+  Page.find(params[:id]).destroy
+  redirect_to pages_path
+
+end
+
+private
+  def pages_params
+  params.require(:page).permit(:title, :author, :body)
+  end
+  # {page: {title: 'foo', author: 'body, body: 'this'}}
+end
